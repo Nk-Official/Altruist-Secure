1c0e3a1824cb04f9edc5f64b39a4d07047d8bdbe
Nk-Official <namrataofficial110@gmail.com>
2020-03-13 10:20:56 +0530

    Setup

---
+++ Altruist Secure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Altruist Secure
+//
+//  Created by Namrata Khanduri on 13/03/20.
+//  Copyright © 2020 Namrata Khanduri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
